bef44cb7f2aa2489d5a791a212c3b6a692881df9
edsonsnunes <edsonnunes05@icloud.com>
2022-07-06 15:35:45 -0300

    05/07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { style } from '@angular/animations';
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'calculator-app-main';
+  public tema = 'tema-1';
+  public mudarTema() {
+    if (this.tema === 'tema-1') {
+      this.tema = 'tema-2';
+    } else if (this.tema === 'tema-2') {
+      this.tema = 'tema-3';
+    } else {
+      this.tema = 'tema-1';
+    }
+  }
 }
